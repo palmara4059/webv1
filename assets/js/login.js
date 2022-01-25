79f128db1608fa87d827bf6fc2b998cbdcf1d8a9
Palmara <70038972+palmara4059@users.noreply.github.com>
2022-01-25 11:33:18 +0100

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -4,7 +4,7 @@ document.getElementById("loginForm").addEventListener("submit",(event)=>{
 
 firebase.auth().onAuthStateChanged((user)=>{
     if(user){
-        location.replace("../.././panels/forum/chat")
+        location.replace("./panels/@me/home.html")
     }
 })
 
@@ -15,24 +15,4 @@ function login(){
     .catch((error)=>{
         document.getElementById("error").innerHTML = error.message
     })
-}
-
-function signUp(){
-    const email = document.getElementById("email").value
-    const password = document.getElementById("password").value
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-    .catch((error) => {
-        document.getElementById("error").innerHTML = error.message
-    });
-}
-
-function forgotPass(){
-    const email = document.getElementById("email").value
-    firebase.auth().sendPasswordResetEmail(email)
-    .then(() => {
-        alert("Reset link sent to your email id")
-    })
-    .catch((error) => {
-        document.getElementById("error").innerHTML = error.message
-    });
 }
