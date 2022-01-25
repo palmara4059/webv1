a67d6ee13bd91eeaf5eb80893208fba6ed939315
Palmara <70038972+palmara4059@users.noreply.github.com>
2022-01-25 13:12:15 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCZexc7R76piOpMT90qXq2cvAklvVpLoIA",
-    authDomain: "fir-donne4.firebaseapp.com",
-    projectId: "fir-donne4",
-    storageBucket: "fir-donne4.appspot.com",
-    messagingSenderId: "854484714742",
-    appId: "1:854484714742:web:9b59d66dac0fbdea3e58d7"
+  apiKey: "AIzaSyC18tNBDGbpqZXN9soUUPsdjZ2lkv5nzUc",
+  authDomain: "hyderium-69.firebaseapp.com",
+  databaseURL: "https://hyderium-69-default-rtdb.firebaseio.com",
+  projectId: "hyderium-69",
+  storageBucket: "hyderium-69.appspot.com",
+  messagingSenderId: "668101176548",
+  appId: "1:668101176548:web:7b7a9cd921b176d4cf10f1",
+  measurementId: "G-NWKR09TWV7"
   };
   firebase.initializeApp(firebaseConfig);
   const db = firebase.database();
+
+  /*const username = prompt("What's your name?");*/
+
 
   document.getElementById("send-message").addEventListener("submit", postChat);
 function postChat(e) {
@@ -16,23 +21,27 @@ function postChat(e) {
   /*const timestamp = Date();*/
   const timestamp = new Date();
 
+  const chatTitre = document.getElementById("chat-titre");
+  const chatArticle = document.getElementById("chat-article");
   const chatTxt = document.getElementById("chat-txt");
   const chatName = document.getElementById("username");
+  const article = chatArticle.value;
+  const titre = chatTitre.value;
   const message = chatTxt.value;
   const username = chatName.value;
 
   chatTxt.value = "";
-
-
   db.ref("messages/" + timestamp).set({
     usr: username,
     date: `${timestamp.toLocaleDateString()} à ${timestamp.toLocaleTimeString()}`,
+    article: article,
+    titre: titre,
     msg: message,
   });
 }
 const fetchChat = db.ref("messages/");
 fetchChat.on("child_added", function (snapshot) {
   const messages = snapshot.val();
-  const msg = "<li>" + `<u>Pseudo :</u> ${messages.usr} &nbsp;&nbsp; <u>Posté le :</u> ${messages.date}` + `<br><br>  <u>Message :</u> <br>` + messages.msg + "</li>";
+  const msg = "<li>" + `Pseudo : ${messages.titre} ${messages.usr} <br> Posté le : ${messages.date}` + `<br><br>  Message : <br>` + messages.msg + "</li>";
   document.getElementById("messages").innerHTML += msg;
 });
