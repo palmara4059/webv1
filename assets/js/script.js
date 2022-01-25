498fa22913a9d1c91e1ded0c5cfea8e354a65a0e
Palmara <70038972+palmara4059@users.noreply.github.com>
2022-01-25 12:26:55 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,39 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyC18tNBDGbpqZXN9soUUPsdjZ2lkv5nzUc",
-  authDomain: "hyderium-69.firebaseapp.com",
-  databaseURL: "https://hyderium-69-default-rtdb.firebaseio.com",
-  projectId: "hyderium-69",
-  storageBucket: "hyderium-69.appspot.com",
-  messagingSenderId: "668101176548",
-  appId: "1:668101176548:web:7b7a9cd921b176d4cf10f1",
-  measurementId: "G-NWKR09TWV7"
+  apiKey: "AIzaSyCkTRS3DOculF4YF5i4vlMHRB8SwfPG-Qc",
+  authDomain: "hyderiu.firebaseapp.com",
+  databaseURL: "https://hyderiu-default-rtdb.firebaseio.com",
+  projectId: "hyderiu",
+  storageBucket: "hyderiu.appspot.com",
+  messagingSenderId: "97533330289",
+  appId: "1:97533330289:web:a1735c6aac38d0c8dbecd0",
+  measurementId: "G-SGZVTMWHCS"
   };
   firebase.initializeApp(firebaseConfig);
   const db = firebase.database();
+  
+  
+  /*const username = prompt("What's your name?");*/
   
   
   document.getElementById("send-message").addEventListener("submit", postChat);
   function postChat(e) {
   e.preventDefault();
   
+  /*const timestamp = Date();*/
   const timestamp = new Date();
-  /*const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
-    
-    const datix = timestamp.options;*/
-    
-    
+  
   const chatTxt = document.getElementById("chat-txt");
+  /*const chatTitre = document.getElementById("chat-titre");*/
   const chatName = document.getElementById("username");
   const message = chatTxt.value;
+  /*const titre = chatTitre.value;*/
   const username = chatName.value;
   chatTxt.value = "";
-
+  
+  
   db.ref("messages/" + timestamp).set({
     usr: username,
-    date: `${timestamp}`,
+    date: `${timestamp.toLocaleDateString()} à ${timestamp.toLocaleTimeString()}`,
     /*titre: titre,*/
     msg: message,
   });
@@ -40,11 +43,16 @@ const firebaseConfig = {
   const messages = snapshot.val();
   
   
-  const msg = `<div class="x-messages"><span class="user">${messages.usr}</span><span class="date">${messages.date}</span><br><span class="txt">${messages.msg}</span></div>`;
-  /*const msg = `<div class="x-messages"><span class="user">${messages.usr}</span><span class="date">${messages.date}</span><br><span class="txt">${messages.msg}</span>  <div class="comments"><div class="comment"><div class="comment-info">` + `<div class="comment-pseudo">${messages.usr}</div><div class="comment-date">${messages.date}</div>` + `</div><div class="comment-comment">${messages.msg}</div></div></div></div>`;*/
+  const msg = `<div class="overlay-scrollbar"><div class="comments"><div class="comment"><div class="comment-info">` + `<div class="comment-pseudo">${messages.usr}</div><div class="comment-date">${messages.date}</div>` + `</div><div class="comment-comment">${messages.msg}</div></div></div></div>`;
   
+  /*const msg = "<li>" + `<u>Pseudo :</u> ${messages.usr} &nbsp;&nbsp; <u>Posté le :</u> ${messages.date}` + `<br><br>  <u>Titre :</u> <br>` + messages.titre +  `<br><br>  <u>Message :</u> <br>` + messages.msg + "</li>";*/
   document.getElementById("messages").innerHTML += msg;
   });
   
+  
+
+  /*const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
+    
+    const datix = timestamp.options;*/
   
   
